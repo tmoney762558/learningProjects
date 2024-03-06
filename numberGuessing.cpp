013988d227c2b581492c85a61a129ec5f42222b7
Tyler Thomas <131617288+tmoney762558@users.noreply.github.com>
2024-03-05 22:56:14 -0600

    Update numberGuessing.cpp

--- numberGuessing.cpp
+++ numberGuessing.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <ctime>
 
+int randNum;
+int guessesCount;
+int guess;
+bool gameOver;
+char replay;
+
+int main() {
+    
+        std::cout << "Welcome to the Random Number Guessing Game!\n";
+        std::cout << "Guess a number 1-10!\n";
+        std::cin >> guess;
+
+        srand(time(NULL));
+        randNum = (rand() % 10) + 1;
+
+        gameOver = false;
+        replay = 'Y';
+
+    do {
+        
+
+        if(guess == randNum) {
+        std::cout << "You guessed correctly!\n";
+        gameOver = true;
+        }
+        else if(guess != randNum) {
+        std::cout << "You guessed incorrectly! Try again.\n";
+        }
+    } while(gameOver == false);
+    return 0;
+}
