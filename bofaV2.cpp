a64bbeda629140d7b313038ea49a3f9e6ae335b0
Tyler Thomas <131617288+tmoney762558@users.noreply.github.com>
2024-03-09 06:45:32 -0600

    Update bofaV2.cpp

--- bofaV2.cpp
+++ bofaV2.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <iomanip>
 
 void checkBalance(double balance);
-void depositMoney(double& balance);
-void withdrawMoney(double& balance);
+void depositMoney(double deposit, double& balance);
+void withdrawMoney(double withdrawal, double& balance);
 void endProgram(bool& exit);
 
 int main() {
     double balance = 0.0;
+    double deposit = 0.0;
+    double withdraw = 0.0;
     int selection;
     bool exit = false;
 
 do {
-    std::cout << "Welcome to Bank of America!\n" << '\n';
+    std::cout << "*Welcome to Bank of America!*\n" << '\n';
     std::cout << "How can we help you today?\n";
     std::cout << "1) Check Balance\n";
     std::cout << "2) Deposit Money\n";
@@ -26,11 +29,11 @@ do {
         break;
     
         case 2:
-        depositMoney(balance);
+        depositMoney(deposit, balance);
         break;
 
         case 3:
-        withdrawMoney(balance);
+        withdrawMoney(withdraw, balance);
         break;
 
         case 4:
@@ -46,32 +49,28 @@ do {
 }
 
 void checkBalance(double balance) {
-    std::cout << "Check Balance\n";
-    std::cout << "Your balance is: $" << balance << ".\n";
+    std::cout << "*Balance*\n";
+    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << ".\n";
 }
-void depositMoney(double& balance) {
-    double deposit;
-    
-    std::cout << "Deposit Money\n";
-    std::cout << "How much money would you like to deposit?\n";
+void depositMoney(double deposit, double& balance) {
+    std::cout << "*Deposit Cash*\n";
+    std::cout << "How much would you like to deposit?\n";
     std::cin >> deposit;
     balance += deposit;
         if(deposit > 0) {
             std::cout << "You deposited $" << deposit << ".\n";
-            std::cout << "Your new balance is $" << balance << ".\n";
+            checkBalance(balance);
         }
         else {
             std::cout << "Invalid value. Deposits cannot be negative\n";
         }
 }
-void withdrawMoney(double& balance) {
-    double withdraw;
-    
-    std::cout << "Withdraw Money\n";
-    std::cout << "How much money would you like to withdraw?\n";
+void withdrawMoney(double withdraw, double& balance) {
+    std::cout << "Withdraw Cash\n";
+    std::cout << "How much would you like to withdraw?\n";
     std::cin >> withdraw;
     if(withdraw > balance) {
-        std::cout << "Invalid value. You only have $" << balance << ".\n";
+        std::cout << "Invalid value. You only have $" << std::setprecision(2) << std::fixed << balance << ".\n";
     }
     else if(withdraw < 0) {
         std::cout << "Invalid value. Withdrawals cannot be negative.\n";
@@ -79,7 +78,7 @@ void withdrawMoney(double& balance) {
     else {
         balance -= withdraw;
         std::cout << "You withdrew $" << withdraw << ".\n";
-        std::cout << "Your new balance is $" << balance << ".\n";
+        checkBalance(balance);
     }
 }
 void endProgram(bool& exit) {
