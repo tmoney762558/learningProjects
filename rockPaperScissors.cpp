b84174b8b56fc17c82fbed92db7e202df6f55478
Tyler Thomas <131617288+tmoney762558@users.noreply.github.com>
2024-03-11 01:05:04 -0500

    Update rockPaperScissors.cpp

--- rockPaperScissors.cpp
+++ rockPaperScissors.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <ctime>
 
-void playerRock(int playerThrow, int aiThrow, char& restart);
-void playerPaper(int playerThrow, int aiThrow, char& restart);
-void playerScissors(int playerThrow, int aiThrow, char& restart);
+void getChoices(int& playerThrow, int& aiThrow);
+void chooseWinner(int playerThrow, int aiThrow, char& restart);
 
 int main() {
     int playerThrow;
@@ -15,30 +14,10 @@ int main() {
 
     std::cout << "*******************************\n";
     std::cout << "Welcome to Rock Paper Scissors!\n";
-    do {
-        std::cout << "Select your throw!\n";
-        std::cout << "1) ROCK\n";
-        std::cout << "2) PAPER\n";
-        std::cout << "3) SCISSORS\n";
-        std::cin >> playerThrow;
-        std::cin.clear();
-        fflush(stdin);
-        aiThrow = (rand() % 3 + 1);
 
-    switch(playerThrow) {
-        case 1:
-            playerRock(playerThrow, aiThrow, restart);
-            break;
-        case 2:
-            playerPaper(playerThrow, aiThrow, restart);
-            break;
-        case 3:
-            playerScissors(playerThrow, aiThrow, restart);
-            break;
-        default:
-            std::cout << "Invalid selection.\n";
-            break;
-    }
+    do {
+        getChoices(playerThrow, aiThrow);
+        chooseWinner(playerThrow, aiThrow, restart);
     }while(restart == 'Y' || restart == 'y');
     
     std::cout << "Thank you for playing!\n";
@@ -46,66 +25,89 @@ int main() {
 
     return 0;
 }
-void playerRock(int playerThrow, int aiThrow, char& restart) {
-    if(playerThrow == aiThrow) {
-        std::cout << "You chose ROCK!\n";
-        std::cout << "The computer chose ROCK!\n";
-        std::cout << "DRAW. Play again!\n";
-    }
-    else if(aiThrow == 2){
-        std::cout << "You chose ROCK!\n";
-        std::cout << "The computer chose PAPER!\n";
-        std::cout << "YOU LOSE!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
-    }
-    else {
-        std::cout << "You chose ROCK!\n";
-        std::cout << "The computer chose SCISSORS!\n";
-        std::cout << "YOU WIN!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
-    }
+void getChoices(int& playerThrow, int& aiThrow) {
+    std::cout << "Select your throw!\n";
+    std::cout << "1) ROCK\n";
+    std::cout << "2) PAPER\n";
+    std::cout << "3) SCISSORS\n";
+    std::cin >> playerThrow;
+    std::cin.clear();
+    fflush(stdin);
+    aiThrow = (rand() % 3 + 1);
 }
-void playerPaper(int playerThrow, int aiThrow, char& restart) {
-    if(playerThrow == aiThrow) {
-        std::cout << "You chose PAPER!\n";
-        std::cout << "The computer chose PAPER!\n";
-        std::cout << "DRAW. Play again!\n";
-    }
-    else if(aiThrow == 3){
-        std::cout << "You chose PAPER!\n";
-        std::cout << "The computer chose SCISSORS!\n";
-        std::cout << "YOU LOSE!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
-    }
-    else {
-        std::cout << "You chose PAPER!\n";
-        std::cout << "The computer chose ROCK!\n";
-        std::cout << "YOU WIN!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
-    }
-}
-void playerScissors(int playerThrow, int aiThrow, char& restart) {
-    if(playerThrow == aiThrow) {
-        std::cout << "You chose SCISSORS!\n";
-        std::cout << "The computer chose SCISSORS!\n";
-        std::cout << "DRAW. Play again!\n";
-    }
-    else if(aiThrow == 1){
-        std::cout << "You chose SCISSORS!\n";
-        std::cout << "The computer chose ROCK!\n";
-        std::cout << "YOU LOSE!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
-    }
-    else {
-        std::cout << "You chose SCISSORS!\n";
-        std::cout << "The computer chose PAPER!\n";
-        std::cout << "YOU WIN!\n";
-        std::cout << "Would you like to play again? (Y/N)\n";
-        std::cin >> restart;
+void chooseWinner(int playerThrow, int aiThrow, char& restart) {
+    switch(playerThrow) {
+        case 1:
+            if(playerThrow == aiThrow) {
+                std::cout << "You chose ROCK!\n";
+                std::cout << "The computer chose ROCK!\n";
+                std::cout << "DRAW. Play again!\n";
+                break;
+            }
+            else if(aiThrow == 2){
+                std::cout << "You chose ROCK!\n";
+                std::cout << "The computer chose PAPER!\n";
+                std::cout << "YOU LOSE!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+            }
+            else {
+                std::cout << "You chose ROCK!\n";
+                std::cout << "The computer chose SCISSORS!\n";
+                std::cout << "YOU WIN!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+            }
+        case 2:
+            if(playerThrow == aiThrow) {
+                std::cout << "You chose PAPER!\n";
+                std::cout << "The computer chose PAPER!\n";
+                std::cout << "DRAW. Play again!\n";
+                break;
+            }
+            else if(aiThrow == 3){
+                std::cout << "You chose PAPER!\n";
+                std::cout << "The computer chose SCISSORS!\n";
+                std::cout << "YOU LOSE!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+            }
+            else {
+                std::cout << "You chose PAPER!\n";
+                std::cout << "The computer chose ROCK!\n";
+                std::cout << "YOU WIN!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+            }
+        case 3:
+            if(playerThrow == aiThrow) {
+                std::cout << "You chose SCISSORS!\n";
+                std::cout << "The computer chose SCISSORS!\n";
+                std::cout << "DRAW. Play again!\n";
+                break;
+            }
+            else if(aiThrow == 1){
+                std::cout << "You chose SCISSORS!\n";
+                std::cout << "The computer chose ROCK!\n";
+                std::cout << "YOU LOSE!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+            }
+            else {
+                std::cout << "You chose SCISSORS!\n";
+                std::cout << "The computer chose PAPER!\n";
+                std::cout << "YOU WIN!\n";
+                std::cout << "Would you like to play again? (Y/N)\n";
+                std::cin >> restart;
+                break;
+        default:
+            std::cout << "Invalid selection.\n";
+            break;
+            }
     }
 }
