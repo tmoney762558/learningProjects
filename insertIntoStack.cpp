a2521263afe651b0e5b2f376f984d40e248564ea
Ty <131617288+tmoney762558@users.noreply.github.com>
2024-03-31 16:20:09 -0500

    Update insertIntoStack.cpp

--- insertIntoStack.cpp
+++ insertIntoStack.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 void Push(int x, int A[], int &top);
-void Pop(int top);
+void Pop(int &top);
 void Print(int top, int A[]);
 
-int top = -1;
-int A[10];
-int x;
 int main() {
+
+    int top = -1;
+    int A[10];
+    int x;    
     int selection;
-    bool running = true;
+    bool running;
 
     std::cout << "Welcome to the Stack Insertion Program!\n";
-        do {
-    std::cout << "What would you like to do?\n";
-    std::cout << "1) Insert Value\n";
-    std::cout << "2) Exit\n";
-    std::cin >> selection;
+    do {
+        std::cout << "What would you like to do?\n";
+        std::cout << "1) Insert Value\n";
+        std::cout << "2) Remove Value\n";
+        std::cout << "3) Print Values\n";
+        std::cout << "4) Exit\n";
+        std::cin >> selection;
 
         switch (selection) {
             case 1:
@@ -26,17 +29,34 @@ int main() {
                 Print(top, A);
                 break;
             case 2:
-                std::cout << "Thank you for visiting.\n";
+                Pop(top);
+                std::cout << "You have removed the top value\n";
+            case 3:
+                Print(top, A);
+                break;
+            case 4:
                 running = false;
+                std::cout << "Thank you for visiting.\n";
+                break;
+            default:
+                std::cout << "Invalid selection. Please pick the number corresponding to your operation.\n";
+                break;
         }
-    } while (running = true);
+    } while (running == true);
     return 0;
 }
 void Push(int x, int A[], int &top) {
     top++;
     A[top] = x;
 }
+void Pop(int &top) {
+     top--;
+}
 void Print(int top, int A[]) {
+    if (top == -1) {
+        std::cout << "The array is empty!\n";
+        return;
+    }
     for (int lastPos = top; lastPos >= 0; lastPos--) {
         std::cout << A[lastPos] << '\n';
     }
